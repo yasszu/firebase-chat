c1af44639721936c32f02f50d3273b8d15522e19
Yasuhiro Suzuki <yssz.vx2xnl@gmail.com>
2017-03-18 17:06:30 +0900

    Configure gradle.

---
+++ app/src/main/java/com/example/firebasechat/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.firebasechat
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v7.widget.Toolbar
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setupToolbar()
+    }
+
+    fun setupToolbar() {
+        val toolbar = findViewById(R.id.toolbar) as Toolbar
+        setSupportActionBar(toolbar)
+    }
+}
